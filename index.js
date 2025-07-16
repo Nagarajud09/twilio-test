2296ea1473cb81961e7c0b4c84f1237d01a9a499
Nagaraju09 <nagarajudevulapally09@gmail.com>
2025-07-16 17:08:01 +0530

    revamped code

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ app.post('/twilio/inbound', (req, res) => {
     const dial = twiml.dial({
         action: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
         method: "POST",
-        // timeout: 20,
-        // record: true,
-        // recordingTrack: "both",
-        // trim: "do-not-trim",
-        // recordingStatusCallback: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
-        // recordingStatusCallbackMethod: "POST",
-        // recordingStatusCallbackEvents: "in-progress completed absent",
+        timeout: 20,
+        statusCallback: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
+        statusCallbackMethod: "POST",
+        record: true,
+        recordingTrack: "both",
+        trim: "do-not-trim",
+        recordingStatusCallback: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
+        recordingStatusCallbackMethod: "POST",
+        recordingStatusCallbackEvents: "in-progress completed absent",
+        statusCallbackEvent: "initiated ringing answered completed"
     });
 
     const called_number = req.query.phoneNumber;
