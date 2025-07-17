dbd4d6afd3cf1d7bac1589706d023bf127bea377
Nagaraju09 <nagarajudevulapally09@gmail.com>
2025-07-17 11:21:45 +0530

    revamped3

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ app.post('/twilio/inbound', (req, res) => {
     twiml.say('Connecting to Runo user.');
 
     const dial = twiml.dial({
-        action: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
-        method: "GET",
+        action: "https://webhook.site/c76a29f9-fa14-40ef-9262-7a3cbf3b82b8",
+        method: "POST",
         timeout: 20,
         statusCallback: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
         statusCallbackMethod: "POST",
         record: true,
         recordingTrack: "both",
         trim: "do-not-trim",
-        recordingStatusCallback: "https://webhook.site/15cec1d8-7693-4962-b823-d02e614e608d",
+        recordingStatusCallback: "https://webhook.site/1baaacd9-d96b-4914-99dc-829b00519ddb",
         recordingStatusCallbackMethod: "POST",
         recordingStatusCallbackEvents: ["in-progress", "completed", "absent"],
         statusCallbackEvent: ["initiated", "ringing", "answered", "completed"]
